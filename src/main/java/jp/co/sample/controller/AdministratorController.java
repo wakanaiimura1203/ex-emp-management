45b005edb0049438efd00ee55a7f5dc927ab863e
iimurawakana <wakana.iimura@rakus-partners.co.jp>
2021-11-04 13:57:18 +0900

    AdministratorControllerを作成したよ

---
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -0,0 +1,33 @@
+package jp.co.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+/**
+ * @author nemur
+ * 管理者登録画面を表示する処理を記述する。
+ *
+ */
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+	
+	@Autowired
+	private AdministratorService administratorService;
+	
+	@ModelAttribute
+	public InsertAdministratorForm setUpInsertAdministratorForm() {
+		return new InsertAdministratorForm();
+	}
+	
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert.html";
+	}
+
+}
---
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -0,0 +1,41 @@
+package jp.co.sample.form;
+
+/**
+ * @author nemur
+ * 管理者情報登録時に使用するフォーム
+ *
+ */
+public class InsertAdministratorForm {
+	
+	private String name;
+	private String mailAddress;
+	private String password;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+				+ "]";
+	}
+	
+	
+}
